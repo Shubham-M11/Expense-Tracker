071fd7989b0a4d944b8fc6b5df2c512db4d4acb4
Shubham-M11 <shubhammaurya9877@gmail.com>
2026-07-25 19:34:54 +0530

    Initialize database on startup

--- app.py
+++ app.py
@@ -118,9 +118,8 @@ def delete(id):
     return redirect(url_for("transactions"))
 
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-
-    with app.app_context():
-        db.create_all()
-
     app.run(debug=True)
