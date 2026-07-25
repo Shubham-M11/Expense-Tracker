06d77f3945bb36f51e04ad9af17731e049d6b298
Shubham-M11 <shubhammaurya9877@gmail.com>
2026-07-25 18:12:03 +0530

    Created Expense model

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
 expenses = []
 
+class Expense(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    expense = db.Column(db.String(100), nullable=False)
+
+    amount = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f"{self.expense}"
 
 @app.route("/")
 def home():
@@ -39,4 +55,8 @@ def view_expenses():
 
 
 if __name__ == "__main__":
+
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
